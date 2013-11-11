a91af20f765bd03f55df4167f6036a78bc9341a5
Ungue <javier.rodriguez@h1labs.com>
2013-11-11 09:23:59 +0100

    Autlinking dot files and tmux initial configuration

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 BUNDLE_DIR="$HOME/.vim/bundle"
+DOT_FILES="vimrc tmux.conf"
+
+BASE_DIR=$(dirname $0)
 
 [ -d "$BUNDLE_DIR" ] || mkdir -p $BUNDLE_DIR
 [ -d "$BUNDLE_DIR/neobundle.vim" ] || git clone git://github.com/Shougo/neobundle.vim $BUNDLE_DIR/neobundle.vim
+
+for f in $DOT_FILES; do
+  [ -f $HOME/$f ] || ln -s $BASE_DIR/$f $HOME/.$f
+done
