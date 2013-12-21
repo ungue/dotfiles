f425b39cd9f8693fe9ae60d79a2f44e00967ce80
Ungue <javier.rodriguez@h1labs.com>
2013-12-21 17:32:07 +0100

    Installs tmuxinator

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BUNDLE_DIR="$HOME/.vim/bundle"
+DROPBOX_DEV="$HOME/Dropbox/Development"
 DOT_FILES="vimrc tmux.conf"
 
 BASE_DIR=$(dirname $0)
@@ -11,3 +12,12 @@ BASE_DIR=$(dirname $0)
 for f in $DOT_FILES; do
   [ -f $HOME/$f ] || ln -s $BASE_DIR/$f $HOME/.$f
 done
+
+[ "$(gem list tmuxinator -i)" == "true" ] || gem install tmuxinator
+
+for yml_file in "$DROPBOX_DEV/tmuxinator/*.yml"; do
+  yml_base_file=$(basename $yml_file)
+  [ -f $HOME/.tmuxinator/$yml_base_file ] || ln -s $yml_file $HOME/.tmuxinator/$yml_base_file
+done
+
+rbenv rehash
