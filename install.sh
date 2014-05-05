2329c4d794f8acebe7d739ff41d3207f77adbca4
Ungue <javier.rodriguez@h1labs.com>
2014-05-05 09:44:26 +0200

    Custom tmux scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BUNDLE_DIR="$HOME/.vim/bundle"
+TMUX_SCRIPTS_DIR="$HOME/.tmux"
 DROPBOX_DEV="$HOME/Dropbox/Development"
 DOT_FILES="vimrc tmux.conf"
 
@@ -13,14 +14,12 @@ for f in $DOT_FILES; do
   [ -f $HOME/$f ] || ln -s $BASE_DIR/$f $HOME/.$f
 done
 
-[ "$(gem list tmuxinator -i)" == "true" ] || gem install tmuxinator
+[ -d "$TMUX_SCRIPTS_DIR" ] || mkdir -p $TMUX_SCRIPTS_DIR
 
-rbenv rehash
+[[ ":$PATH:" == *":$TMUX_SCRIPTS_DIR:"* ]] || export PATH=$PATH:$TMUX_SCRIPTS_DIR
 
-tmuxinator doctor
-
-for yml_file in "$DROPBOX_DEV/tmuxinator/*.yml"; do
-  yml_base_file=$(basename $yml_file)
-  [ -f $HOME/.tmuxinator/$yml_base_file ] || ln -s $yml_file $HOME/.tmuxinator/$yml_base_file
+for tmux_script_file in $(ls $DROPBOX_DEV/tmux/*.sh); do
+  tmux_script_base_file=$(basename $tmux_script_file)
+  [ -f $TMUX_SCRIPTS_DIR/$tmux_script_base_file ] || ln -s $tmux_script_file $TMUX_SCRIPTS_DIR/$tmux_script_base_file 
 done
 
