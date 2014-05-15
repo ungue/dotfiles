499b08e01013642cc129e2c6460f7fdbacade4a7
Ungue <javier.rodriguez@h1labs.com>
2014-05-15 22:25:30 +0200

    Silver searcher installation

--- install.sh
+++ install.sh
@@ -16,10 +16,13 @@ done
 
 [ -d "$TMUX_SCRIPTS_DIR" ] || mkdir -p $TMUX_SCRIPTS_DIR
 
-[[ ":$PATH:" == *":$TMUX_SCRIPTS_DIR:"* ]] || export PATH=$PATH:$TMUX_SCRIPTS_DIR
-
 for tmux_script_file in $(ls $DROPBOX_DEV/tmux/*.sh); do
   tmux_script_base_file=$(basename $tmux_script_file)
   [ -f $TMUX_SCRIPTS_DIR/$tmux_script_base_file ] || ln -s $tmux_script_file $TMUX_SCRIPTS_DIR/$tmux_script_base_file 
 done
 
+if [ "$(uname)" == "Darwin" ]; then
+  brew install the_silver_searcher
+else
+  sudo apt-get install silversearcher-ag 
+fi
